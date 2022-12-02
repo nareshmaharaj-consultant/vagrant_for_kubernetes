4e5dfc50917afcc7219195b5ecaa5235d8727291
Naresh Maharaj <naresh.maharaj@icloud.com>
2022-12-02 12:36:01 +0000

    Added lua for playing around Added a variable for the k8s version

--- setup-k8s.sh
+++ setup-k8s.sh
@@ -1,5 +1,8 @@
 cd ~
+VER=1.19.12
 PWD=`pwd`
+IP_MASTER="10.0.0.10"
+IP=`hostname -i | awk '{print $2}'`
 
 sudo modprobe br_netfilter
 
@@ -43,14 +46,38 @@ sudo apt-get install -y apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubelet=1.25.1-00 kubeadm=1.25.1-00 kubectl=1.25.1-00
+#sudo apt-get install -y kubelet=1.25.1-00 kubeadm=1.25.1-00 kubectl=1.25.1-00
+sudo apt-get install -y kubelet=${VER}-00 kubeadm=${VER}-00 kubectl=${VER}-00
 sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
 sudo systemctl restart containerd
 wget https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
 sudo apt-get install bash-completion
+sudo apt-get install jq
+kubectl completion bash >> ~/.bashrc
 
 rm cni-plugins-linux-amd64*
 rm containerd-*
 rm runc.amd64*
 
-kubectl completion bash >> ~/.bashrc
+# kubeadm on master only
+if [ $IP = $IP_MASTER ]
+then
+  sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=$IP
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+  kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.2/manifests/tigera-operator.yaml
+  kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.2/manifests/custom-resources.yaml
+  kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+  echo "run the following command: [ watch kubectl get pods -n calico-system ]"
+fi
+
+# Lua
+curl -R -O http://www.lua.org/ftp/lua-5.4.4.tar.gz
+tar zxf lua-5.4.4.tar.gz
+cd lua-5.4.4
+make all test
+sudo make install
