813ff49c4b92ed47de4bd79d33782861b98ad74e
Naresh Maharaj <42941747+nareshmaharaj-consultant@users.noreply.github.com>
2025-04-20 08:23:21 +0100

    Update _start.sh

--- _start.sh
+++ _start.sh
@@ -1,8 +1,15 @@
-start=`date +%s`
+start=$(date +%s)
+
 vagrant up | tee log.txt
 vagrant plugin install vagrant-hostmanager
 vagrant hostmanager
-end=`date +%s`
 
-runtime=$((end-start))
-echo Time taken to complete: $runtime in seconds
+end=$(date +%s)
+
+# Default to 0 if start or end is empty
+start=${start:-0}
+end=${end:-0}
+
+# Now safe to do math
+runtime=$((end - start))
+echo "Time taken to complete: $runtime seconds"
