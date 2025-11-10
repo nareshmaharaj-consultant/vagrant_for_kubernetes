255e108a44b25ec3872b7acc448a137cf91bb9b5
Naresh Maharaj <42941747+nareshmaharaj-consultant@users.noreply.github.com>
2025-11-10 08:18:56 +0000

    Update sandbox image in containerd confifle file

--- setup-k8s.sh
+++ setup-k8s.sh
@@ -61,6 +61,7 @@ sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
 sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+sudo sed -i 's/sandbox_image \= \"registry.k8s.io\/pause:3.6\"/sandbox_image \= \"registry.k8s.io\/pause:3.9\"/g' /etc/containerd/config.toml
 sudo systemctl restart containerd
 wget https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
 sudo apt-get install bash-completion -y
